04afee729656f3b64ca99a2887c94f7e4f513ccb
THEDEATHBOT <jafroth@gmail.com>
2016-11-01 17:39:36 -0700

    Document SmartDashboardAble

--- src/org/usfirst/frc/team997/robot/SmartDashboardAble.java
+++ src/org/usfirst/frc/team997/robot/SmartDashboardAble.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team997.robot;
 
+/** An interface providing access to a single function, `smartDashboard()`, that allows
+ * uniform access to classes that want access to the Smart Dashboard. */
 public interface SmartDashboardAble {
+	/** Log information to the Smart Dashboard. */
 	public void smartDashboard();
 }
