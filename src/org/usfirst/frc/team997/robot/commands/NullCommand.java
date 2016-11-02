35d2a3065ee8fb7409700db91e6a0d6e07d2e97f
THEDEATHBOT <jafroth@gmail.com>
2016-11-01 17:34:01 -0700

    Document NullCommand

--- src/org/usfirst/frc/team997/robot/commands/NullCommand.java
+++ src/org/usfirst/frc/team997/robot/commands/NullCommand.java
@@ -2,6 +2,10 @@ package org.usfirst.frc.team997.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 
+/** This Command serves to do nothing -- to mark an action as having no effect,
+ * but not being `null`.  The problem with `null` is that usages of it result
+ * in NullPointerExceptions, but this instead implements its functionality as
+ * doing nothing and immediately ending. */
 public class NullCommand extends Command {
 	protected void initialize() {}
 	protected void execute() {}
