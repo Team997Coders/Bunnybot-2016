9001e4116ecc29fdfbbf9bd6533d95eb742ec705
THEDEATHBOT <jafroth@gmail.com>
2016-11-01 17:38:04 -0700

    Add comments for encoders in DriveTrain, `driveVoltage()`

--- src/org/usfirst/frc/team997/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team997/robot/subsystems/DriveTrain.java
@@ -5,12 +5,16 @@ import org.usfirst.frc.team997.robot.SmartDashboardAble;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class DriveTrain extends Subsystem implements SmartDashboardAble {
-	/*TODO add motors */
+	/*TODO add motors... two Victors */
+	/*TODO add encoder for the left Victor*/
 	public DriveTrain() {
 		/*TODO setup motors, use RobotMap values*/
 	}
 
-	public void smartDashboard() {}
+	public void smartDashboard() {/*TODO -- log encoder values*/}
 
 	protected void initDefaultCommand() {/*TODO Set the default command*/}
+
+	/** Sets the voltages of the motors to the respective arguments. */
+	public void driveVoltage(double left, double right) {/*TODO*/}
 }
