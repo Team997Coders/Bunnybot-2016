2c99479ea721afce6ede83f1ad28ca74d0fda0f9
THEDEATHBOT <jafroth@gmail.com>
2016-11-11 17:42:25 -0800

    Added Button(TriggerToggle)

--- src/org/usfirst/frc/team997/robot/OI.java
+++ src/org/usfirst/frc/team997/robot/OI.java
@@ -1,12 +1,15 @@
 package org.usfirst.frc.team997.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+	private Button triggerToggle;
 	private Joystick left, right, xbox;
 	private boolean isXbox;
 	public OI() {
@@ -14,6 +17,7 @@ public class OI {
 		right = new Joystick(1);
 		xbox = new Joystick(2);
 		isXbox = false;
+		triggerToggle = new JoystickButton(left, 1);
 	}
 	
 	public double getLeftY() {
